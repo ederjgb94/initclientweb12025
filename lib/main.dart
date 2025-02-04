4c024c45927f2d261a21ba48d3ee1dd7178d7d14
“ederjgb94” <ederjbg94@gmail.com>
2025-02-04 12:50:12 -0600

    Add pet management features: create, update, and delete pets with dialog inputs

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final int _counter = 0;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  String domain = 'y8wsckyqiv.sharedwithexpose.com';
 
   Future<List<dynamic>> _incrementCounter() async {
-    var url = Uri.https('hmtikjipor.sharedwithexpose.com', 'api/pets');
+    var url = Uri.https(domain, 'api/pets');
     var response = await http.get(url);
     // print('Response status: ${response.statusCode}');
     // print('Response body: ${response.body}');
@@ -49,8 +53,30 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> removePet(int id) async {
-    var url = Uri.https('hmtikjipor.sharedwithexpose.com', 'api/pets/$id');
+    var url = Uri.https(domain, 'api/pets/$id');
     await http.delete(url);
+    setState(() {});
+  }
+
+  Future<void> addPet(String name, String age) async {
+    var url = Uri.https(domain, 'api/pets');
+    var response = await http.post(url, body: {'name': name, 'age': age});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    setState(() {});
+  }
+
+  Future<Map<String, dynamic>> showPet(int id) async {
+    var url = Uri.https(domain, 'api/pets/$id');
+    var response = await http.get(url);
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
+
+  Future<void> updatePet(int id, String name, String age) async {
+    var url = Uri.https(domain, 'api/pets/$id');
+    var response = await http.put(url, body: {'name': name, 'age': age});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
     setState(() {});
   }
 
@@ -75,6 +101,72 @@ class _MyHomePageState extends State<MyHomePage> {
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     title: Text(snapshot.data[index]['name']),
+                    onTap: () async {
+                      //muestra un dialogo utilizando el showPet
+                      Map<String, dynamic> pet =
+                          await showPet(snapshot.data[index]['id']);
+                      TextEditingController nameControllerUpdate =
+                          TextEditingController(
+                        text: pet['name'].toString(),
+                      );
+                      TextEditingController ageControllerUpdate =
+                          TextEditingController(
+                        text: pet['age'].toString(),
+                      );
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Pet info'),
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('id: ${pet['id']}'),
+                                TextField(
+                                  controller: nameControllerUpdate,
+                                  decoration: InputDecoration(
+                                    hintText: 'Name',
+                                  ),
+                                ),
+                                TextField(
+                                  controller: ageControllerUpdate,
+                                  decoration: InputDecoration(
+                                    hintText: 'Age',
+                                  ),
+                                ),
+                                Text('Create: ${pet['created_at']}'),
+                                Text('Update: ${pet['updated_at']}'),
+                              ],
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancelar'),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  await updatePet(
+                                    pet['id'],
+                                    nameControllerUpdate.text,
+                                    ageControllerUpdate.text,
+                                  );
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text('Pet updated'),
+                                    ),
+                                  );
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Guardar'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
                     trailing: IconButton(
                       onPressed: () async {
                         await removePet(snapshot.data[index]['id']);
@@ -111,7 +203,60 @@ class _MyHomePageState extends State<MyHomePage> {
             }
           }),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          //crea un input dialog para agregar una mascota
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Add a pet'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                      ),
+                    ),
+                    TextField(
+                      controller: ageController,
+                      decoration: InputDecoration(
+                        labelText: 'Age',
+                      ),
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      await addPet(
+                        nameController.text,
+                        ageController.text,
+                      );
+                      nameController.clear();
+                      ageController.clear();
+                      //pon un snackbar
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Pet added'),
+                        ),
+                      );
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
